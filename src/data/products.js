9fa380e672a07f87b1d757ba8d0200e973ec8363
ansarimofid <ansarimofid@gmail.com>
2026-06-04 13:39:31 +0530

    Initial commit

---
+++ src/data/products.js
@@ -0,0 +1,93 @@
+// Products shown on the homepage. Add new ones here — the homepage maps over this list.
+export const products = [
+  {
+    name: 'Blinkstore',
+    href: 'http://blinkstore.in/',
+    logo: '/img/blinkstore.svg',
+    desc: 'Launch your online merchandise store for free!',
+  },
+  {
+    name: 'Letterflix',
+    href: 'https://letterflix.com/',
+    logo: '/img/lettertoxyz.svg',
+    desc: 'Write and Send Letters Online | Letter Delivery',
+  },
+  {
+    name: 'Mockey',
+    href: 'https://mockey.co/',
+    logo: '/img/mockey.svg',
+    desc: 'Super awesome mockup generator for apparel, accessories, mobile etc. 100+ products and still counting',
+  },
+  {
+    name: 'BharatMetaverse',
+    href: 'https://www.bharatmetaverse.com/',
+    logo: '/img/bmeta.svg',
+    desc: 'Metaverse That Represents India in The Virtual World.',
+  },
+  {
+    name: 'BabyCorn',
+    href: 'https://babycorn.co/',
+    logo: '/img/babycorn.png',
+    desc: 'Personalised apparel and accessories for kids.',
+  },
+  {
+    name: 'FOMO Store',
+    href: 'http://fomostore.in',
+    logo: '/img/fomo-store.svg',
+    desc: '[WIP] Vibe up with amazing unlimited (limited-edition) products from FOMO store.',
+  },
+  {
+    name: 'BihariSwag',
+    href: 'https://bihariswag.com/',
+    logo: '/img/bihari.svg',
+    desc: 'Quirky apparel and accessory brand with Bihari vibes.',
+  },
+  {
+    name: 'Snaccx',
+    href: 'https://snaccx.com/',
+    logo: '/img/snaccx.svg',
+    desc: 'Clothing and accessories for snacc people.',
+  },
+  {
+    name: 'Gloset',
+    href: 'https://gloset.in/',
+    logo: '/img/gloset.svg',
+    desc: '[WIP] An expriment to test and learn about Indian apparel ecosystem',
+  },
+  {
+    name: 'Real Apple',
+    href: 'https://apple.byxyz.net/',
+    logo: '/img/apple.png',
+    desc: 'The Real Apple that has been never seen before.',
+  },
+  {
+    name: 'FOMO Wall',
+    href: 'http://fomowall.omg.run/',
+    logo: '/img/fomo-wall.svg',
+    desc: 'Vibe up with amazing unlimited & unique wallpapers.',
+  },
+  {
+    name: 'Dulo',
+    href: 'https://dulo.byxyz.net/',
+    logo: '/img/dulo.svg',
+    desc: 'Where designers share their work for feedback.',
+  },
+  {
+    name: 'Stumbleonmusic',
+    href: 'https://stumbleonmusic.com/',
+    logo: '/img/stumble.svg',
+    desc: 'Discover music through stories',
+  },
+  {
+    name: 'iWish',
+    href: 'https://iwish.byxyz.net/',
+    logo: '/img/iwish.svg',
+    desc: 'It’s kinda like, Twitter for your wishes.',
+  },
+  {
+    name: 'Random Gradients',
+    href: 'https://rg.byxyz.net/',
+    logo: '/img/random-gradients.svg',
+    desc: 'Generate unlimited random gradients. Explore, Save, Use it in your code.',
+  },
+];
